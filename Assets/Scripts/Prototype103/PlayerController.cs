2583b96eade755fdcaf7bddd21d47076bb7bd8d3
LAPTOPCOLIN\User <colin@subat.de>
2022-11-25 14:36:47 +0100

    added prototype103 scene and scripts

---
+++ Assets/Scripts/Prototype103/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float forwardInput;
+    public float speed;
+    public float turnSpeed;
+    public Vector3 force;
+
+    private Animator _playerAnim;
+    private Rigidbody _playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerAnim = GetComponent<Animator>();
+        _playerRB = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
+
+        _playerAnim.SetFloat("Run", forwardInput);
+
+        if (forwardInput != 0 || horizontalInput != 0)
+        {
+            _playerAnim.SetBool("Walk", true);
+        }
+        else
+        {
+            _playerAnim.SetBool("Walk", false);   
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRB.AddForce(force, ForceMode.Impulse);
+            _playerAnim.SetTrigger("Jump");
+        }
+        
+    }
+}
